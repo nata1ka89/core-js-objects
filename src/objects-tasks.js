56f0272aacb81bae7f7373f74a2e4365615df080
nata1ka <nata1ka.yurevich@gmail.com>
2025-02-24 16:04:50 +0300

    feat: add part 2 of the task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -80,8 +80,8 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
@@ -95,8 +95,9 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  const newArr = Object.keys(obj);
+  return newArr.length === 0;
 }
 
 /**
@@ -115,8 +116,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -129,8 +130,14 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const word = [];
+  Object.entries(lettersObject).forEach(([key, value]) => {
+    value.forEach((index) => {
+      word[index] = key;
+    });
+  });
+  return word.join('');
 }
 
 /**
@@ -147,8 +154,35 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const cash = {
+    25: 0,
+    50: 0,
+    100: 0,
+  };
+  return queue.every((element) => {
+    if (element === 25) {
+      cash[25] += 1;
+    } else if (element === 50) {
+      if (cash[25] < 1) {
+        return false;
+      }
+      cash[50] += 1;
+      cash[25] -= 1;
+    } else if (element === 100) {
+      if (cash[25] >= 1 && cash[50] >= 1) {
+        cash[100] += 1;
+        cash[25] -= 1;
+        cash[50] -= 1;
+      } else if (cash[25] >= 3) {
+        cash[100] += 1;
+        cash[25] -= 3;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  });
 }
 
 /**
